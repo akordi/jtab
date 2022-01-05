74c78c0611e72e642a18ad610eb117ef2123e0b1
Oskars Pakers <oskars.pakers@gmail.com>
2022-01-05 13:34:10 +0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  devtool: false,
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "jtab.js",
+    library: {
+      name: "jtab",
+      type: "var"
+    },
+  },
+};
